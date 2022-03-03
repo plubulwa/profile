5340fde17b5a93e8f761eb60318eafdc32b65d28
plubulwa <s3857381@student.rmit.edu.au>
2022-03-03 19:15:22 +1100

    working with branches etc and working out the navigation

---
+++ profile/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Profile - Peter Lubulwa</h1>
+      <br />
+      <h2>Hold Music</h2>
+      <p>Its gonna get better</p>
+     
+    </div>
+  );
+}
+
+export default App;
